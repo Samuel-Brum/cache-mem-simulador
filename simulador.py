e595a1003fde59ffcad54f4ae37a9182d5efc3af
SamuelBrum <samuelbrummartins@gmail.com>
2023-12-01 16:43:47 -0300

    cria índice

--- simulador.py
+++ simulador.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 cache_size = sys.argv[1]
 line_size = sys.argv[2]
@@ -25,34 +26,53 @@ hex_dict = {'0': '0000',
             'E': '1110', 
             'F': '1111'}
 
-bin_dict = dict(map(reversed, hex_dict.items()))
+bin_dict = dict(map(reversed, hex_dict.items())) # dicionário invertido
 
 def hex_2_bin(hex: str) -> str:
+  """Converte de hexadecimal para binário
+  
+  Args:
+    hex (str): string contendo número hexadecimal
+  Returns:
+    string contendo número binário
+  """
+
   bin = ''
   for char in hex:
     bin += hex_dict[char]
   return bin
 
 def bin_2_hex(bin: str) -> str:
+  """Converte de binário para hexadecimal
+
+  Args:
+    bin (str): string contendo número binário
+  Returns:
+    string contendo número hexadecimal
+  """
+
   hex = ''
-  for char in bin: 
-    hex += bin_dict[char]
+  separated = [(bin[i:i+4]) for i in range(0, len(bin), 4)] # separa grupos de 4 bits do endereço binário
+  for four_bits in separated: 
+    hex += bin_dict[four_bits]
   return hex
 
 bin_values = [hex_2_bin(hex) for hex in hex_values] # gera lista com valores de memória em binário (como string)
 
-class cache(cache_size, line_size, associativity):
-  """Summary
-  
-  Args:
-    cache_size (int):
-    line_size (int):
-    group_size (int):
+offset = math.ceil(math.log2(line_size))
 
+bin_offset = [bin[:len(bin) - offset].rjust(32, '0') for bin in bin_values] # remove offset do cache
 
-  Returns:
+hex_offset = ["0x" + bin_2_hex(bin) for bin in bin_offset] # converte de volta para hexadecimal (como string)
 
-  """
+class Cache(cache_size, line_size, associativity):
   no_of_indexes = cache_size / (line_size * associativity)
+  
+  initial_value = [['0', ''] for i in range(no_of_indexes)]
+  key = [str(i).rjust(3, '0') for i in range(no_of_indexes)]
+  index = {k:v for (k, v) in zip(key, initial_value)} # serve como banco de memória da cache
+
+
+
 
 
